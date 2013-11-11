529db98b62c82bc46f7c097f2b18106b59ea2b6b
CrossTyreck <cohamilt@asu.edu>
2013-11-10 20:22:33 -0700

    Removed bar graph circles; Replaced task list image with a more appropriate table object (needs work); Resized the body header content for index.cshtml;

--- LotBanking2/DueDiligence_v1/Scripts/bargraph-v1.js
+++ LotBanking2/DueDiligence_v1/Scripts/bargraph-v1.js
@@ -1,36 +1,38 @@
-$(function(){
-	
-	var input = $('.input'),
+//do not edit. This is retarded and the slightest editing could make it blow up. -Chris
+
+$(function () {
+
+    var input = $('.input'),
 		bar = $('.bar'),
 		bw = bar.width(),
 		percent = bar.find('.percent'),
 		circle = bar.find('.circle'),
-		ps =  percent.find('span'),
+		ps = percent.find('span'),
 		cs = circle.find('span'),
 		name = 'rotate';
-	
-	input.on('keydown', function(e){
-		if (e.keyCode == 13){
-			var t = $(this), val = t.val();
-			if (val >=0 && val <= 100){
-				var w = 100-val, pw = (bw*w)/100,
+
+    input.on('keydown', function (e) {
+        if (e.keyCode == 13) {
+            var t = $(this), val = t.val();
+            if (val >= 0 && val <= 100) {
+                var w = 100 - val, pw = (bw * w) / 100,
 					pa = {
-						width: w+'%'
+					    width: w + '%'
 					},
-					cw = (bw-pw)/2,
+					cw = (bw - pw) / 2,
 					ca = {
-						left: cw
+					    left: cw
 					}
-				ps.animate(pa);
-				cs.text(val+'%');
-				circle.animate(ca, function(){
-					circle.removeClass(name)
-				}).addClass(name);	
-			} else {
-				alert('range: 0 - 100');
-				t.val('');
-			}
-		}
-	});
-	
+                ps.animate(pa);
+                cs.text(val + '%');
+                circle.animate(ca, function () {
+                    circle.removeClass(name)
+                }).addClass(name);
+            } else {
+                alert('range: 0 - 100');
+                t.val('');
+            }
+        }
+    });
+
 });
--- LotBanking2/DueDiligence_v1/Scripts/init.js
+++ LotBanking2/DueDiligence_v1/Scripts/init.js
@@ -1,36 +1,36 @@
-$(function(){
-	
-	var input = $('.input'),
-		bar = $('.bar'),
-		bw = bar.width(),
-		percent = bar.find('.percent'),
-		circle = bar.find('.circle'),
-		ps =  percent.find('span'),
+$(function () {
+
+    var input = $('.input'),
+		bar1 = $('.bar1'),
+		bw = bar1.width(),
+		percent = bar1.find('.percent'),
+		circle = bar1.find('.circle'),
+		ps = percent.find('span'),
 		cs = circle.find('span'),
 		name = 'rotate';
-	
-	input.on('keydown', function(e){
-		if (e.keyCode == 13){
-			var t = $(this), val = t.val();
-			if (val >=0 && val <= 100){
-				var w = 100-val, pw = (bw*w)/100,
+
+    input.on('keydown', function (e) {
+        if (e.keyCode == 13) {
+            var t = $(this), val = t.val();
+            if (val >= 0 && val <= 100) {
+                var w = 100 - val, pw = (bw * w) / 100,
 					pa = {
-						width: w+'%'
+					    width: '$' + w
 					},
-					cw = (bw-pw)/2,
+					cw = (bw - pw) / 2,
 					ca = {
-						left: cw
+					    left: cw
 					}
-				ps.animate(pa);
-				cs.text(val+'%');
-				circle.animate(ca, function(){
-					circle.removeClass(name)
-				}).addClass(name);	
-			} else {
-				alert('range: 0 - 100');
-				t.val('');
-			}
-		}
-	});
-	
+                ps.animate(pa);
+                cs.text('$' + val);
+                circle.animate(ca, function () {
+                    circle.removeClass(name)
+                }).addClass(name);
+            } else {
+                alert('range: 0 - 100');
+                t.val('');
+            }
+        }
+    });
+
 });
