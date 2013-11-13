f818869f7be4d25d7695626ab279a10f92a61967
CrossTyreck <cohamilt@asu.edu>
2013-11-12 20:35:06 -0700

    Duplicated bar graph JavaScript and css elements. Very poor coding but gets the job done. In my defense I did try to bind the javascript bar element in an object then run the function on that element so that the code does not need to be duplicated but this was taking to long and not working.

--- LotBanking2/DueDiligence_v1/Scripts/bargraph-v1.js
+++ LotBanking2/DueDiligence_v1/Scripts/bargraph-v1.js
@@ -1,13 +1,13 @@
 $(function () {
 
-    var input = $('.input'),
-		bar = $('.bar'),
-		bw = bar.width(),
-		percent = bar.find('.percent'),
-		circle = bar.find('.circle'),
-		ps = percent.find('span'),
-		cs = circle.find('span'),
-		name = 'rotate';
+    var input = $('.input1'),
+        bar = $('.bar1'),
+    bw = bar.width(),
+    percent = bar.find('.percent'),
+    circle = bar.find('.circle'),
+    ps = percent.find('span'),
+    cs = circle.find('span'),
+    name = 'rotate';
 
     input.on('keydown', function (e) {
         if (e.keyCode == 13) {
@@ -15,9 +15,91 @@ $(function () {
             if (val >= 0 && val <= 100000) {
 
                 // val/1000 for val = 100000 made sense Monday Night
-                var w = 100 - (val/1000), pw = (bw * w) / 100,
+                var w = 100 - (val / 1000), pw = (bw * w) / 100,
 					pa = {
-                        //Outputs width to HTML for circle use
+					    //Outputs width to HTML for circle use
+					    width: w + '%'
+					},
+					cw = (bw - pw) / 2,
+					ca = {
+					    left: cw
+					}
+                ps.animate(pa);
+                //changed % to $ no problem. Need to remember to clean and build solution
+                cs.text('$' + val);
+                circle.animate(ca, function () {
+                    circle.removeClass(name)
+                }).addClass(name);
+            } else {
+                alert('range: 0 - 100000');
+                t.val('');
+            }
+        }
+    });
+
+});
+
+$(function () {
+
+    var input = $('.input2'),
+        bar = $('.bar2'),
+    bw = bar.width(),
+    percent = bar.find('.percent'),
+    circle = bar.find('.circle'),
+    ps = percent.find('span'),
+    cs = circle.find('span'),
+    name = 'rotate';
+
+    input.on('keydown', function (e) {
+        if (e.keyCode == 13) {
+            var t = $(this), val = t.val();
+            if (val >= 0 && val <= 100000) {
+
+                // val/1000 for val = 100000 made sense Monday Night
+                var w = 100 - (val / 1000), pw = (bw * w) / 100,
+					pa = {
+					    //Outputs width to HTML for circle use
+					    width: w + '%'
+					},
+					cw = (bw - pw) / 2,
+					ca = {
+					    left: cw
+					}
+                ps.animate(pa);
+                //changed % to $ no problem. Need to remember to clean and build solution
+                cs.text('$' + val);
+                circle.animate(ca, function () {
+                    circle.removeClass(name)
+                }).addClass(name);
+            } else {
+                alert('range: 0 - 100000');
+                t.val('');
+            }
+        }
+    });
+
+});
+
+$(function () {
+
+    var input = $('.input3'),
+        bar = $('.bar3'),
+    bw = bar.width(),
+    percent = bar.find('.percent'),
+    circle = bar.find('.circle'),
+    ps = percent.find('span'),
+    cs = circle.find('span'),
+    name = 'rotate';
+
+    input.on('keydown', function (e) {
+        if (e.keyCode == 13) {
+            var t = $(this), val = t.val();
+            if (val >= 0 && val <= 100000) {
+
+                // val/1000 for val = 100000 made sense Monday Night
+                var w = 100 - (val / 1000), pw = (bw * w) / 100,
+					pa = {
+					    //Outputs width to HTML for circle use
 					    width: w + '%'
 					},
 					cw = (bw - pw) / 2,
