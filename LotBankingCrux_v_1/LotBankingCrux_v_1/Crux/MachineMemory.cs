9bdbba8853d40c04b08e2528acfa5c7c0cd84564
CruxJustin <Jrederic@asu.edu>
2013-12-16 18:02:39 -0700

    no message

---
+++ LotBankingCrux_v_1/LotBankingCrux_v_1/Crux/MachineMemory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LotBankingCrux_v_1.Crux
+{
+    public class MachineMemory
+    {
+        static int userId;
+        static int projectId;
+        public MachineMemory()
+        {
+        }
+    }
+}
